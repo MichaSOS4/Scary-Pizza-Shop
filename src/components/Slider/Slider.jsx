57a3f3f34407a222d8458659796197a480d4bc30
michasos4 <michaoo3@wp.pl>
2025-10-08 14:10:23 +0200

    adding slider

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,55 +1,108 @@
 import styles from './Slider.module.css'
-
-
+import { useRef ,useState,useEffect} from 'react'
+import pizza1 from '../../images/pizza1.jpg'
+import pizza2 from '../../images/pizza2.jpg'
+import pizza3 from '../../images/pizza3.jpg'
+import {ArrowRight,ArrowLeft  } from 'lucide-react'
 function Slider(){
 
+    const [counterSlide,setCounterSlide] = useState(0)
+    
+    const slideIsideSlide = useRef(null)
+     
+    
+
+    
     const slides = [
         {
             title: 'Margarita',
-            url:''
+            url: pizza1
 
         },
         {
             title: 'Fungi',
-            url:''
+            url: pizza2
 
         },
         {
             title: 'Capriciosa',
-            url:''
+            url: pizza3
 
         },
         {
             title: 'Ingleciana',
-            url:''
+            url: pizza1
 
         },
         {
             title: 'Hawajska',
-            url:''
+            url: pizza2
 
         },
         {
             title: 'Salami',
-            url:''
+            url: pizza3
 
         },
         {
             title: 'Scary Monster',
-            url:''
+            url: pizza1
 
         },
         {
             title: 'Meat Hedgegog',
-            url:''
+            url: pizza2
 
         },
         {
             title: 'UltraCheese',
-            url:''
+            url: pizza3
 
         },
     ]
+    const changeSlideRight = ()=>{
+        
+    setCounterSlide(prevSlide => prevSlide += 1)
+    if(counterSlide >= 2){
+        setCounterSlide(0)
+    }
+    
+    
+        
+        
+        
+        
+    }
+    const changeSlideLeft = ()=>{
+        setCounterSlide(prevSlide => prevSlide -= 1)
+    if(counterSlide <= 0){
+        setCounterSlide(2)
+    }
+        
+        
+        
+    }
+    useEffect(()=>{
+        
+        switch(counterSlide){
+            case 1: slideIsideSlide.current.style.left = `calc(-100%)`;
+            break;
+            case 2: slideIsideSlide.current.style.left = `-200%`;
+            break;
+            case 0:slideIsideSlide.current.style.left = `0`;
+            break;
+            
+
+
+
+
+        }
+        
+        
+
+
+    },[counterSlide])
+
 
 
 return(
@@ -57,12 +110,25 @@ return(
         <h1 className = {styles.sliderTitle}>Najczęściej wybierane pozycje</h1>
 
         <div className = {styles.sliderBar}>
-        <span>left</span>
+        <span onClick={changeSlideLeft}><ArrowLeft color='rgb(235, 129, 7)' /></span>
         <div className = {styles.slides}>
+            <div ref={slideIsideSlide} className={styles.slidesInside}>
 
+                {slides.map((slide,idx)=>{
+                
+                
+                return (
+                    <div style={{backgroundImage : `url('${slide.url}')` }}  key={idx} className = {styles.singleSlide}>
+                        <h4>{slide.title}</h4>
+                        
+                    </div>
+                )
+            })}
+            </div>
+            
 
         </div>
-        <span>right</span>
+        <span onClick={changeSlideRight}><ArrowRight color='rgb(235, 129, 7)' /></span>
         </div>
 
 
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,13 @@
 import Navbar from "../../components/Navbar/Navbar"
 import styles from './HomePage.module.css'
 import Banner from "../../components/Banner/Banner"
+import Slider from "../../components/Slider/Slider"
 function HomePage() {
   return (
     <div className={styles.homePageWrapper}>
       <Navbar></Navbar>
       <Banner></Banner>
+      <Slider></Slider>
     </div>
   )
 }
